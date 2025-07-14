7ba185328261ceea439058d322ce066ba680f95f
04042015 <m.iggopramulia14@gmail.com>
2025-07-14 18:58:03 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,19 @@
 import { POSInterface } from "@/components/POSInterface";
+import { Link } from "react-router-dom";
 
 const Index = () => {
-  return <POSInterface />;
+  return (
+    <>
+      <POSInterface />
+      <div className="fixed bottom-4 right-4">
+        <Link to="/shift-report">
+          <button className="px-4 py-2 bg-blue-600 text-white rounded shadow">
+            📊 Laporan Shift
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Index;
